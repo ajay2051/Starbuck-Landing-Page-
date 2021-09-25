5cb92b58c629aa56b09a00b85a6af275d5f0cb95
ajay2051 <33621153+ajay2051@users.noreply.github.com>
2021-09-25 23:35:59 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function imgSlider(anything) {
+	document.querySelector(".image").src = anything;
+}
+
+function changeCircleColor(color) {
+	// const circle = document.getElementById("circle").value;
+	// circle.style.background = color;
+	document.getElementById("circle").style.background = color;
+}
+
+function changeTextColor(text) {
+	document.getElementById("starbucks").style.color = text;
+}
+
+function changeBgColor(bg) {
+	document.getElementById("button").style.background = bg;
+}
